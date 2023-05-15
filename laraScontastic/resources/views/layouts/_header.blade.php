d59c965c72fcc19f5592777eeb717801fcc28215
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-15 12:51:07 +0200

    implementata la sidenav

--- laraScontastic/resources/views/layouts/_header.blade.php
+++ laraScontastic/resources/views/layouts/_header.blade.php
@@ -1,11 +1,25 @@
 <header>
-    <select class="category">
-        <option value="prova 1">Prova 1</option>
-        <option value="prova 1">Prova 1</option>
-        <option value="prova 1">Prova 1</option>
-        <option value="prova 1">Prova 1</option>
-        <option value="prova 1">Prova 1</option>
-    </select>
+    <script>
+        /* Set the width of the side navigation to 250px */
+        function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        /* Set the width of the side navigation to 0 */
+        function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        } 
+    </script>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+
+    <!-- Use any element to open the sidenav -->
+    <span onclick="openNav()"><img src="images/toggle_icon.png"></span>
     <!-- inizio logo -->
     <img src="images/scontastic_draft.png">
     <!-- fine logo -->
