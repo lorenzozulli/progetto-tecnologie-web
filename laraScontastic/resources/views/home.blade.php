9691da315792701bf65b720d0583faaafdc74662
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-15 15:22:19 +0200

    Implementazione della home quasi completata

--- laraScontastic/resources/views/home.blade.php
+++ laraScontastic/resources/views/home.blade.php
@@ -14,50 +14,55 @@
                 @include('layouts/_header')
         </div>
         <!-- header section end-->
-
+      <div class="mega_container">
         <!-- banner section start -->
-        <div class="banner_section layout_padding">
-            <div class="container">
-                     <div class="carousel-item">
-                        <div class="row">
-                           <div class="col-sm-12">
-                              <h1 class="banner_title">Start <br>Shopping NOW</h1>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="buynow_bt"><a href="https://www.google.com/">Buy Now</a></div>
-                  </div>
-               </div>
-            </div>
+        <div class="banner_bg">
+         <div class="frame">
+            <h1>INIZIA ORA <br>LO SHOPPING</h1>
+            <div class="bt"><a href="https://www.google.com/">Compra adesso</a></div>
+         </div>
          </div>
          <!-- banner section end -->
-      </div>
       <!-- banner bg main end -->
 
         <!-- Aziende section start -->
         <div class="carousel_wrapper">
             <div class="card">
                <a href="#" target="blank">
-               <img class="img" src="#">
-               <h1>Test</h1>
+                  <img class="img" src="#">
+                  <h3>Test</h3>
+               </a>
+            </div>
+
+            <div class="card">
+               <a href="#" target="blank">
+                  <img class="img" src="#">
+                  <h3>Test</h3>
+               </a>
+            </div>
+
+            <div class="card">
+               <a href="#" target="blank">
+                  <img class="img" src="#">
+                  <h3>Test</h3>
                </a>
             </div>
         </div>
-         <div class="buynow_bt"><a href="https://www.google.com/">Tutte le Aziende</a></div>
-      </div>
+         <div class="bt"><a href="https://www.google.com/">Tutte le Aziende</a></div>
       <!-- aziende section end -->
 
         <!-- codes explanation section start -->
         <div class="codes_explanation">
-            <article class="explanation">
-               <h1 class="aziende_title">Cosa sono i codici?</h1>
-               <p class="prova">Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br>
-                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </article>
+               <h1 class="title">Cosa sono i codici?</h1>
+               <p>Nell'ambito del commercio i codici sconto servono da un lato alle aziende, che vogliono fidelizzare i loro clienti, e ai clienti per usufruire di vantaggi
+                   in ambito puramente monetario oppure di sconti di vario genere e tipologia.</p>
+               <h3>Sconto in percentuale:</h3>
+               <p>Sono una semplice riduzione di prezzo in percentuale sul costo originario dell'articolo</p>
+               <h3>Sconto fisso:</h3>
+               <p>Sono una semplice riduzione di prezzo determinata dall'importo monetario sul costo originario dell'articolo</p>
         </div>
         <!-- codes explanation section end -->
-
+      </div>
         <!-- footer section start -->
         <div id="menu">
                 @include('layouts/_footer')
