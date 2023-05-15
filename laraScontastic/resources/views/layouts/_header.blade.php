d55eae9bc5154d164d8071ef938d33a7a1e7af4e
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-15 13:20:08 +0200

    Aggiunte le categorie nella sidenav (ovviamente solo a livello di frontend)

--- laraScontastic/resources/views/layouts/_header.blade.php
+++ laraScontastic/resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
     <script>
         /* Set the width of the side navigation to 250px */
         function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.width = "500px";
         }
 
         /* Set the width of the side navigation to 0 */
@@ -12,10 +12,33 @@
     </script>
         <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
+            <h1 class="category">CATEGORIE</h1>
+            <a href="#">Elettronica e informatica</a>
+            <a href="#">Abbigliamento e Accessori</a>
+            <a href="#">Scarpe</a>
+            <a href="#">Voli</a>
+            <a href="#">Vacanze e Hotel</a>
+            <a href="#">Casa e Giardino</a>
+            <a href="#">Videogiochi</a>
+            <a href="#">Giocattoli</a>
+            <a href="#">Parchi Divertimento</a>
+            <a href="#">Spesa Online</a>
+            <a href="#">Auto e Moto</a>
+            <a href="#">Assicurazioni</a>
+            <a href="#">Cosmetici e Profumi</a>
+            <a href="#">Salute e Farmacia</a>
+            <a href="#">Sport e Fitness</a>
+            <a href="#">Energia</a>
+            <a href="#">Telefono, Internet e TV</a>
+            <a href="#">Animali</a>
+            <a href="#">Neonati e Bambini</a>
+            <a href="#">Finanza</a>
+            <a href="#">Musica, Libri e Film</a>
+            <a href="#">Servizi Business e Ufficio</a>
+            <a href="#">Concerti, Cinema e Cultura</a>
+            <a href="#">Idee Regalo e Gift Card</a>
+            <a href="#">Trasporti</a>
+            <a href="#">Ristoranti</a>
         </div>
 
     <!-- Use any element to open the sidenav -->
