7c0c39c4af813a41c17eddb5ca9af06a29eda1ab
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-16 10:21:33 +0200

    Aggiunti i link nel footer

--- laraScontastic/resources/views/layouts/_footer.blade.php
+++ laraScontastic/resources/views/layouts/_footer.blade.php
@@ -10,4 +10,13 @@
         <p><a href="{{ route('faq') }}">Hai delle domande? clicca qui per vedere le nostre FAQ</a></p>
     </div>
     <!-- fine FAQ -->
+    <!-- inizio about -->
+    <div class="about">
+        <p>Progetto di Tecnologie Web di: 
+            <a href="#">Maicol Lanni</a>, 
+            <a href="https://github.com/MarioMaio98">Mario Maio</a>, 
+            <a href="https://github.com/Lorenzo-Marrone">Lorenzo Marrone</a>, 
+            <a href="https://github.com/lorenzozulli">Lorenzo Zulli</a>.</p>
+    </div>
+    <!-- fine about -->
 </footer>
