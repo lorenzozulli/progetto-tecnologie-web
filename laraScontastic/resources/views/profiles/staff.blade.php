0003ec6b46f56a545dbe1815b8b0ad5a7735a4c3
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-20 11:19:57 +0200

    Iniziata inplementazione delle pagine dei vari utenti

--- laraScontastic/resources/views/profiles/staff.blade.php
+++ laraScontastic/resources/views/profiles/staff.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts/base')
 @section('content')
         <!-- staff section start -->
-            <div class="mega_container">
-                <p>ciao sono la pagina dello staff</p>
-            </div>
+        <div class="mega_container">
+            <h3>Area Staff</h3>
+            <p>Benvenuto {{ Auth::user()->name }} {{ Auth::user()->surname }}</p>
+            <p>Seleziona la funzione da attivare</p>
+        </div>
         <!-- staff section end -->
 @endsection
--- laraScontastic/resources/views/profiles/user.blade.php
+++ laraScontastic/resources/views/profiles/user.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts/base')
 @section('content')
         <!-- user section start -->
-            <div class="mega_container">
-                <p>ciao sono la pagina dell'user</p>
-            </div>
+        <div class="mega_container">
+            <h3>Area User</h3>
+            <p>Benvenuto {{ Auth::user()->name }} {{ Auth::user()->surname }}</p>
+            <p>Seleziona la funzione da attivare</p>
+        </div>
         <!-- user section end -->
 @endsection
