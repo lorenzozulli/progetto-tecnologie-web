5deaeabf7113b16f2fa91bb10557b5ddcc9d1f29
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-23 14:44:52 +0200

    aggiunta una prima implementazione della pagina dell'offerta, rinominata aziende in azienda

--- laraScontastic/resources/views/offerta.blade.php
+++ laraScontastic/resources/views/offerta.blade.php
@@ -2,7 +2,29 @@
 @section('content')
         <!-- offerta section start -->
             <div class="mega_container">
-                <p>ciao sono la pagina dell'offerta</p>
+                <!-- immagine azienda section start -->
+                <div class="image_logo">
+                    <img src="#">
+                </div>
+                <!-- immagine azienda section end -->
+                
+                <!-- coupon section start -->
+                <div class="coupon">
+                    @guest
+                    <h3>Registrati per scoprire il codice</h3>
+                    @endguest
+                    <!-- collegamento con il coupon -->
+                </div>
+                <!-- coupon section end -->
+
+                <div class="the_other_stuff">
+                    <h1><a href="#">Acquisici</a> il coupon come</h1>
+                    <!-- da mettere il nome e il cognome dell'utente -->
+                    <h1>Il coupon scade il..</h1>
+                    <!-- inserire la data di scadenza -->
+                    <h1>Descrizione</h1>
+                    <!-- da inserire la descrizione del coupon-->
+                </div>
             </div>
         <!-- offerta section end -->
 @endsection
