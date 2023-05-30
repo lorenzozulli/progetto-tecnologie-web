ade86d4bae30767cf7a834dde9589e226fff4e3f
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-30 11:38:07 +0200

    Continua l'implementazione delle barre di ricerca :)

--- laraProject/resources/views/lista-aziende.blade.php
+++ laraProject/resources/views/lista-aziende.blade.php
@@ -3,9 +3,9 @@
         <!-- lista aziende section start -->
         <div class="mega_container">
         <h1 style="text-align:center"> LISTA AZIENDE </h1>
-            @if(Auth::user()->livello == 3)
-                <h3><a href="{{ route('aggiunta-azienda') }}">Aggiungi Azienda</a></h3>
-            @endif
+            {{--@if(Auth::user()->livello == 3)--}}
+                <!--<h3><a href="{{ route('aggiunta-azienda') }}">Aggiungi Azienda</a></h3>-->
+            {{--@endif--}}
                 <div id="content">
                     @isset($companies)
                     @foreach ($companies as $company)
@@ -15,20 +15,18 @@
                             @if($company->logo == NULL)
                             <img class="img" src="{{ asset('images/loghi-aziende/non_disponibile.png') }}">
                             @else
-                            <img src="data:image/png/jpeg;base64,{{ base64_encode($tuple['logo']) }}" alt="Immagine Azienda">
+                            <img src="{{-- asset('images/loghi-aziende'.$company->logo --}}" alt="Immagine Azienda">
                             @endif
                             <div class="info">
                                 <h1 class="title_carousel_item">{{ $company->nome }}</h1>
                             </div>
-                            @if(Auth::user()->livello == 3)
-                                    
-                                        
-                                <form action="{{ route('delete-azienda',  $company['id'])}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
+                            {{--@if(Auth::user()->livello == 3)--}}                                   
+                                <!--<form action="{{ route('delete-azienda',  $company['id'])}}" method="POST">
+                                    {{--@csrf--}}
+                                    {{--@method('DELETE')--}}
                                     <button type="submit" class="modifiche" onclick="return confirm('Sei sicuro di voler eliminare questa offerta?')" ><img src="{{ asset('images/delete.png') }}"></button>
-                                </form>
-                            @endif   
+                                </form>-->
+                            {{--@endif--}}
                         </a>
                         </div>                    
                     </div>
