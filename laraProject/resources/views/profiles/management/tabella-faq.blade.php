7eca0b972c1115a67304bebf333b78853d39553b
Lorenzo-Marrone <lorenzo.marrone.lm@gmail.com>
2023-05-31 11:11:44 +0200

    tabella faq

--- laraProject/resources/views/profiles/management/tabella-faq.blade.php
+++ laraProject/resources/views/profiles/management/tabella-faq.blade.php
@@ -1,8 +1,38 @@
 @extends('layouts/base')
 @section('content')
-        <!-- tabella FAQ section start -->
+        <!-- tabella faqs section start -->
         <div class="mega_container">
-            <p>ciao sono la pagina con la tabella delle FAQ</p>
+            <h2>tabella faq</h2>
+        
+        <!-- tabella faqs -->    
+<table>
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>domanda</th>
+            <th>risposta</th>
+            <th>azioni</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($faqs as $faq)
+            <tr>
+                <td>{{ $faq->id }}</td>
+                <td>{{ $faq->domanda}}</td>
+                <td>{{ $faq->risposta }}</td>
+                <td>
+                    <a href="{{-- --}}">Modifica</a>
+                    <form action="{{-- --}}" method="POST"> 
+                         @csrf
+                         @method('DELETE')
+                         <button type="submit" onclick="return confirm('Sei sicuro di voler eliminare questa faq?')" ><img class="modifiche" src="{{ asset('images/delete.png') }}"></button>
+                   </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
         </div>
-        <!-- tabella FAQ section end -->
+        <!-- tabella faqs section end -->
 @endsection
