fe2cf2dd4600a793aaaa7cd86c16bca3ed4fc9fe
Lorenzo-Marrone <lorenzo.marrone.lm@gmail.com>
2023-05-31 11:15:04 +0200

    tabella utenti

--- laraProject/resources/views/profiles/management/tabella-utenti.blade.php
+++ laraProject/resources/views/profiles/management/tabella-utenti.blade.php
@@ -2,7 +2,63 @@
 @section('content')
         <!-- tabella utenti section start -->
         <div class="mega_container">
-            <p>ciao sono la pagina con la tabella degli utenti</p>
+            <h2>tabella utenti</h2>
+        
+        <!-- tabella utenti-->    
+<table>
+    <thead>
+        <tr>
+            <th>username</th>
+            <th>nome</th>
+            <th>cognome</th>
+            <th>eta</th>
+            <th>genere</th>
+            <th>livello</th>
+            <th>telefono</th>
+            <th>email</th>
+            <th>azioni</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->nome}}</td>
+                <td>{{ $user->cognome }}</td>
+                <td>{{ $user->eta }}</td>
+                <td>{{ $user->genere }}</th>
+                <td>{{ $user->livello }}</th>
+                <td>{{ $user->telefono }}</th>
+                <td>{{ $user->email }}</th>
+                <td>
+                    @if ($user->livello === '3')
+                    <!-- Mostra solo al profilo admin -->
+                    <form action="{{-- --}}" method="POST"> 
+                         @csrf
+                         @method('DELETE')
+                         <button type="submit" onclick="return confirm('Sei sicuro di voler eliminare questo utente?')" ><img class="modifiche" src="{{ asset('images/delete.png') }}"></button>
+                            Elimina
+                         </button>
+                   </form>
+                   @else
+                   <!-- Mostra solo al profilo staff -->
+                   <a href= "{{--  --}}">Modifica</a>
+                   <a href= "{{--  --}}">Cancella</a>
+                   @if ($user->livello === '2')
+                       <form action="{{-- --}}" method="POST">
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit"> Crea offerta<button>
+                       </form>
+                   @endif
+                   @endif
+                
+                </td>
+           </tr>
+        @endforeach  
+    </tbody>
+</table>
+
         </div>
         <!-- tabella utenti section end -->
 @endsection
