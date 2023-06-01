50716b0b90591ff7f66030beccb758eaec991e07
Lorenzo-Marrone <lorenzo.marrone.lm@gmail.com>
2023-06-01 10:34:49 +0200

    modifica faq funziona, aggiunta rotta sempre di modifica faq e modificato altro

--- laraProject/resources/views/profiles/management/modifica-faq.blade.php
+++ laraProject/resources/views/profiles/management/modifica-faq.blade.php
@@ -1,8 +1,51 @@
 @extends('layouts/base')
 @section('content')
-        <!-- modifica FAQ section start -->
-        <div class="mega_container">
-            <p>ciao sono la pagina di modifica delle FAQ</p>
+
+    <!-- Modifica FAQ section start -->
+    <div class="mega_container">
+        <p>Ciao, questa è la pagina di modifica delle faq</p>
+    </div>
+    <div class="container-contact">
+        <h3>Modifica FAQ</h3>
+        <div class="wrap-contact">
+
+            {{ Form::open(array('route' => ['modifica-faq', $id], 'class' => 'contact-form')) }}
+            
+
+           <!-- Domanda -->
+            <div class="wrap-input">
+                {{ Form::label('domanda', 'Domanda', ['class' => 'label-input']) }}
+                {{ Form::text('domanda', '', ['class' => 'input', 'id' => 'domanda']) }}
+                @if ($errors->first('domanda'))
+                <ul class="errors">
+                    @foreach ($errors->get('domanda') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+                @endif
+            </div>
+
+            <!-- Risposta -->
+            <div class="wrap-input">
+                {{ Form::label('risposta', 'Risposta', ['class' => 'label-input']) }}
+                {{ Form::textarea('risposta', '', ['class' => 'input', 'id' => 'risposta']) }}
+                @if ($errors->first('risposta'))
+                <ul class="errors">
+                    @foreach ($errors->get('risposta') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+                @endif
+            </div>
+
+            <!-- Bottone di modifica -->
+            <div class="container-form-btn">
+                {{ Form::submit('Modifica', ['class' => 'form-btn1']) }}
+            </div>
+
+            {{ Form::close() }}
         </div>
-        <!-- modifica FAQ section end -->
+    </div>
+    <!-- Modifica FAQ section end -->
+
 @endsection
