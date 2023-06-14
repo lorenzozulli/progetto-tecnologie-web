331e0991e14dfa225a2c238657af5495296afece
Mario maio <57944979+mandro98@users.noreply.github.com>
2023-06-14 10:59:42 +0200

    Okay

--- laraProject/public/js/ajax.js
+++ laraProject/public/js/ajax.js
@@ -1,6 +1,21 @@
 
 
  
+ var availableTags = [];
+ $.ajax({
+   method: "GET",
+   url: offerteAjax,
+   success: (response)=>{
+       console.log(response)
+       startAutoComplete(response);
+   }
+ });
+ function startAutoComplete(availableTags){
+   $( "#search_offer" ).autocomplete({
+  
+     source: availableTags
+   });
+ }
  
  var availableTags = [];   //viene creata la variabile e inizializzata come array vuoto
   $.ajax({    //apre la chiamata AJAX 
